9733b16ec1386d9f8e08508a99071dd4da37ce2d
Abhishek <abhishek.kabi.21@gmail.com>
2026-01-19 13:06:31 +0530

    Added new features

--- src/App.jsx
+++ src/App.jsx
@@ -189,6 +189,9 @@ function App() {
 
       <div className="footer">
         <p>&copy; Copyright All Rights Reserved 2025.</p>
+        <p style={{ color: "gray", margin: "0" }}>
+          <i class="fa-solid fa-globe"></i> Global
+        </p>
       </div>
     </>
   );
