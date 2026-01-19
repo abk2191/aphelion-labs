9cdc5ef856a17ec825823b0da318526ef5c8f7d6
Abhishek <abhishek.kabi.21@gmail.com>
2026-01-20 00:15:42 +0530

    Added new features

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -82,7 +82,9 @@ function Sidebar({
 
         <div className="sidebar-items">
           <div className="menu-item">
-            <p style={{ color: "midnightblue" }}>✨️ The UI Lab 🧪</p>
+            <p style={{ color: "midnightblue" }}>
+              ✨️&nbsp;&nbsp; The UI Lab 🧪
+            </p>
             <button className="toggle-button" onClick={() => toggleMenu("ui")}>
               {menuVisibility === "ui" ? (
                 <i class="fa-solid fa-angle-up"></i>
@@ -100,7 +102,7 @@ function Sidebar({
           </div>
 
           <div className="menu-item">
-            <p style={{ color: "midnightblue" }}>📧 Contact</p>
+            <p style={{ color: "midnightblue" }}>📧&nbsp;&nbsp; Contact</p>
             <button
               className="toggle-button"
               onClick={() => toggleMenu("contact")}
