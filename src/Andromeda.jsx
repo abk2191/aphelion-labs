58fb6eea6da4c5c257983c7167842adf2564c206
Abhishek <abhishek.kabi.21@gmail.com>
2026-01-20 17:19:47 +0530

    Added new features

--- src/Andromeda.jsx
+++ src/Andromeda.jsx
@@ -31,6 +31,27 @@ function Andromeda() {
           <div className="andromeda-screenshot-notes"></div>
           <div className="andromeda-screenshot-notes-open"></div>
         </div>
+
+        <div className="wrapper">
+          <div className="andromeda-page-heading">
+            <h2>NOTES</h2>
+            <p>
+              To help you stay centered as you capture your thoughts, Andromeda
+              offers a beautifully simple space to organize your notes. <br />{" "}
+              <br />
+              You can easily prioritize your ideas by pinning or unpinning them,
+              ensuring your most important tasks always remain on track. <br />
+              <br /> To make the experience truly yours, you can also customize
+              your workspace with a variety of beautiful colors for a more
+              personal, intentional feel.
+            </p>
+
+            <div className="note-features">
+              <p>👉 Pin important notes</p>
+              <p>👉 Give your notes colors</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
