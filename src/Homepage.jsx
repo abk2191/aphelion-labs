8e051315d24519e55cfbee5d5202a6c3f089e863
Abhishek <abhishek.kabi.21@gmail.com>
2026-01-21 23:25:06 +0530

    Added new features

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -58,40 +58,66 @@ function Homepage() {
                   <p>Full Stack</p>
                 </div>
               </div> */}
-              <div className="tech-stack-div-one">
-                <div className="tech-wrapper">
-                  <i class="fa-brands fa-react" style={{ color: "cyan" }}></i>{" "}
-                  <p>React</p>
+              <div className="tech-stack-div-wrapper-wrapper">
+                <div className="tech-stack-div-one">
+                  <div className="tech-wrapper">
+                    <i
+                      class="fa-brands fa-react"
+                      style={{ color: "midnightblue" }}
+                    ></i>{" "}
+                    <p>React</p>
+                  </div>
+                  <div className="tech-wrapper">
+                    <i
+                      class="fa-brands fa-js"
+                      style={{ color: "midnightblue" }}
+                    ></i>
+                    <p>JavaScript</p>
+                  </div>
+                  <div className="tech-wrapper">
+                    <i
+                      class="fa-brands fa-html5"
+                      style={{ color: "midnightblue" }}
+                    ></i>
+                    <p>HTML 5</p>
+                  </div>
+                  <div className="tech-wrapper">
+                    <i
+                      class="fa-brands fa-git-alt"
+                      style={{ color: "midnightblue" }}
+                    ></i>
+                    <p>Git</p>
+                  </div>
                 </div>
-                <div className="tech-wrapper">
-                  <i class="fa-brands fa-js" style={{ color: "gold" }}></i>
-                  <p>JavaScript</p>
-                </div>
-                <div className="tech-wrapper">
-                  <i class="fa-brands fa-html5" style={{ color: "orange" }}></i>
-                  <p>HTML 5</p>
-                </div>
-                <div className="tech-wrapper">
-                  <i class="fa-brands fa-git-alt" style={{ color: "red" }}></i>
-                  <p>Git</p>
-                </div>
-              </div>
-              <div className="tech-stack-div-one">
-                <div className="tech-wrapper">
-                  <i class="fa-brands fa-python" style={{ color: "blue" }}></i>
-                  <p>Python</p>
-                </div>
-                <div className="tech-wrapper">
-                  <i class="fa-brands fa-java" style={{ color: "blue" }}></i>
-                  <p>Java</p>
-                </div>
-                <div className="tech-wrapper">
-                  <i class="fa-brands fa-docker" style={{ color: "green" }}></i>
-                  <p>Docker</p>
-                </div>
-                <div className="tech-wrapper">
-                  <i class="fa-solid fa-globe" style={{ color: "navy" }}></i>
-                  <p>Full Stack</p>
+                <div className="tech-stack-div-two">
+                  <div className="tech-wrapper">
+                    <i
+                      class="fa-brands fa-python"
+                      style={{ color: "midnightblue" }}
+                    ></i>
+                    <p>Python</p>
+                  </div>
+                  <div className="tech-wrapper">
+                    <i
+                      class="fa-brands fa-java"
+                      style={{ color: "midnightblue" }}
+                    ></i>
+                    <p>Java</p>
+                  </div>
+                  <div className="tech-wrapper">
+                    <i
+                      class="fa-brands fa-docker"
+                      style={{ color: "midnightblue" }}
+                    ></i>
+                    <p>Docker</p>
+                  </div>
+                  <div className="tech-wrapper">
+                    <i
+                      class="fa-solid fa-globe"
+                      style={{ color: "midnightblue" }}
+                    ></i>
+                    <p>Full Stack</p>
+                  </div>
                 </div>
               </div>
             </div>
