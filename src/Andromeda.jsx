02aede8db4cbf0de8239be7e9df005e343606e0f
Abhishek <abhishek.kabi.21@gmail.com>
2026-01-22 12:40:42 +0530

    Added new features

--- src/Andromeda.jsx
+++ src/Andromeda.jsx
@@ -82,13 +82,11 @@ function Andromeda() {
               designed to keep your schedule and thoughts in perfect harmony.
               Whether you prefer a bird's-eye view of your entire year or a
               focused monthly layout, switching between perspectives is seamless
-              and simple. One of its standout features is the "thought of the
-              day" option, allowing you to anchor your daily reflections
-              directly alongside your appointments. <br /> <br /> You can also
-              easily add or view specific events with just a quick tap on any
-              date and also get notifications. We’ve focused on creating a
-              clutter-free experience that helps you manage your time while
-              leaving room for personal growth.
+              and simple. <br /> <br /> You can also easily add or view specific
+              events with just a quick tap on any date and also get
+              notifications. We’ve focused on creating a clutter-free experience
+              that helps you manage your time while leaving room for personal
+              growth.
             </p>
           </div>
         </div>
