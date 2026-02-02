dfcb860b5fddc40425a38f6867a616f40506068c
Abhishek <abhishek.kabi.21@gmail.com>
2026-02-02 17:12:00 +0530

    Added new features

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -24,7 +24,7 @@ function Navbar({
         {currentTheme === "dark" ? (
           <i className="fa-solid fa-sun"></i>
         ) : (
-          <i className="fa-solid fa-moon"></i>
+          <i class="fa-solid fa-cloud-moon"></i>
         )}
       </button>
 
